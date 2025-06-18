8e9691b10964856fafc0d0751c8fd0a1cd5e0b66
girish-kanjiyani7 <girish.kanjiyani5040@gmail.com>
2025-06-18 14:26:49 -0400

    j

--- movie-react/src/App.jsx
+++ movie-react/src/App.jsx
@@ -1,17 +1,11 @@
 import React from "react"
+import './index.css'
 
-
-const App= ()=> {
-return( 
-    <main>
-        <div className="pattern"/>
-        <div className="wrapper">
-            <header>
-                <h1>
-                    Find Movies that you'll Enjoy without the hassle
-                </h1>
-            </header>
-        </div>
-    </main>
-)
-}
+export default function App() {
+    return (
+      <div className="bg-red-500 text-white p-4 text-3xl">
+        If you see red, Tailwind is working.
+      </div>
+    );
+  }
+  
