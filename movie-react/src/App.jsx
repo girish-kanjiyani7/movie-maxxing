7431f36f7e045ccd8fc48b7e6e57d798689dd245
girish-kanjiyani7 <girish.kanjiyani5040@gmail.com>
2025-06-18 13:32:18 -0400

    k.

---
+++ movie-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const App= ()=> {
+return( 
+    <main>
+        <div className="pattern"/>
+        <div className="wrapper">
+            <header>
+                <h1>
+                    Find Movies that you'll Enjoy without the hassle
+                </h1>
+            </header>
+        </div>
+    </main>
+)
+}
