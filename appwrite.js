b634ea399d83beda1c92d9c3fdc53e16f1d50ac1
girish-kanjiyani7 <girish.kanjiyani5040@gmail.com>
2025-06-25 19:02:13 -0400

    remove debug logs for Appwrite env vars

--- appwrite.js
+++ appwrite.js
@@ -4,11 +4,6 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-// Debug logs (add these just after the constants)
-console.log('PROJECT_ID:', PROJECT_ID);
-console.log('DATABASE_ID:', DATABASE_ID);
-console.log('COLLECTION_ID:', COLLECTION_ID);
-
 const client = new Client()
 .setEndpoint('https://nyc.cloud.appwrite.io/v1')
 .setProject(PROJECT_ID)
